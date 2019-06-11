47a3243e655a090137bbb6442a06327dbbc08ba7
Amit Biswas <biswas1992@hotmail.com>
2019-06-11 02:43:25 -0400

    Initial Commit

---
+++ APIRequest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APIRequest
+//
+//  Created by Maze Geek on 6/11/19.
+//  Copyright © 2019 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
